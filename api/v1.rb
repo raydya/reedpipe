be765528529fdc2cca4fefb6f52fd9bcbb8ee403
raydya <dya.ilaempire@gmail.com>
2016-05-19 13:45:33 +0800

    i

--- api/v1.rb
+++ api/v1.rb
@@ -1,27 +1,27 @@
 module Reedpipe
-	module Api
-		class V1 < Grape::API
-			format :json
-			version ['v2','v1'], using: :path, format: :json, cascade: true
+  module Api
+    class V1 < Grape::API
+      format :json
+      version ['v2','v1'], using: :path, format: :json, cascade: true
       prefix :api
 
-			desc 'Returns the current API version ,v1'
-			get do
-				{version: 'v1'}
-			end
+      desc 'Returns the current API version ,v1'
+      get do
+        {version: 'v1'}
+      end
 
-			get :hello do
-				{hello: 'world'}
-			end
+      get :hello do
+        {hello: 'world'}
+      end
 
-			get :sample do
-				user = OpenStruct.new
+      get :sample do
+        user = OpenStruct.new
         user.id = 1
         user.name = 'testname'
-				present user, :with => Reedpipe::Entities::SampleEntity
+        present user, :with => Reedpipe::Entities::SampleEntity
         body()
-			end
+      end
 
-		end
-	end
+    end
+  end
 end
